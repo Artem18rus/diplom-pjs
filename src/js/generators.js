85ff5d6ce98ae38f713fe2b9c532013f162d8f94
Artem18 <urart88@mail.ru>
2022-10-17 21:04:38 +0400

    3

--- src/js/generators.js
+++ src/js/generators.js
@@ -5,7 +5,7 @@ import Magician from './characters/Magician';
 import Vampire from './characters/Vampire';
 import Undead from './characters/Undead';
 import Daemon from './characters/Daemon';
-
+import Team from './Team';
 /**
  * Формирует экземпляр персонажа из массива allowedTypes со
  * случайным уровнем от 1 до maxLevel
@@ -24,15 +24,15 @@ export function* characterGenerator(allowedTypes, maxLevel) {
   }
 }
 
-// const playerTypes = [Bowman, Swordsman, Magician]; // доступные классы игрока
+//const playerTypes = [Bowman, Swordsman, Magician]; // доступные классы игрока
 // //console.log(characterGenerator(playerTypes, 2));
 // const playerGenerator = characterGenerator(playerTypes, 2); // в данном примере персонажи игрока могут быть 1 или 2-ого уровней
 // //console.log(playerGenerator.next())
 // const character1 = playerGenerator.next().value; // случайный персонаж из списка playerTypes с уровнем 1 или 2
-// console.log(character1.type); // magician
+// //console.log(character1.type); // magician
 
 // const character2 = playerGenerator.next().value; // ещё один случайный персонаж
-// console.log(character2.level); // 1
+// //console.log(character2.level); // 1
 // //character2.type; // swordsman
 
 /**
@@ -43,6 +43,14 @@ export function* characterGenerator(allowedTypes, maxLevel) {
  * @returns экземпляр Team, хранящий экземпляры персонажей. Количество персонажей в команде - characterCount
  * */
 export function generateTeam(allowedTypes, maxLevel, characterCount) {
-  // TODO: write logic here
+  // TODO: write logic here 
+  return playerTypes;
 }
 
+const playerTypes = [Bowman, Swordsman, Magician]; // доступные классы игрока
+const team = generateTeam(playerTypes, 3, 4); // массив из 4 случайных персонажей playerTypes с уровнем 1, 2 или 3
+
+console.log(team);
+// console.log(team.characters[0].level) // 3
+// console.log(team.characters[1].level) // 3
+// console.log(team.characters[2].level) // 1
